7cfdd179ed5be62ae26b33decf4ffb5a1684cd50
JWI <jwi@j-w-i.org>
2025-12-16 15:23:47 +1100

    quick fix for emoji's not working properly

--- Neocord/CacheManagers/EmojiCache.swift
+++ Neocord/CacheManagers/EmojiCache.swift
@@ -7,9 +7,10 @@ final class EmojiCache {
 
     private let memoryCache = NSCache<NSString, UIImage>()
     private let cacheQueue = DispatchQueue(label: "emoji.cache.queue")
+
     private let cacheDirectory: String = {
-        let dirs = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)
-        return dirs.first ?? NSTemporaryDirectory()
+        NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first
+        ?? NSTemporaryDirectory()
     }()
 
     private init() {}
@@ -18,51 +19,43 @@ final class EmojiCache {
         let cacheKey = id as NSString
         let filePath = cacheDirectory + "/" + id + ".png"
 
-        // Memory cache
-        if let cachedImage = memoryCache.object(forKey: cacheKey) {
-            completion(cachedImage)
+        if let cached = memoryCache.object(forKey: cacheKey) {
+            DispatchQueue.main.async { completion(cached) }
             return
         }
 
-        // Disk cache
-        if let diskData = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
-           let image = UIImage(data: diskData) {
+        if let data = try? Data(contentsOf: URL(fileURLWithPath: filePath)),
+           let image = UIImage(data: data) {
             memoryCache.setObject(image, forKey: cacheKey)
-            completion(image)
+            DispatchQueue.main.async { completion(image) }
             return
         }
 
-        // Fetch from CDN
         guard let url = URL(string: "https://cdn.discordapp.com/emojis/\(id).png?v=1") else {
-            completion(nil)
+            DispatchQueue.main.async { completion(nil) }
             return
         }
 
-        URLSessionCompat.shared.dataTask(with: URLRequest(url: url)) { [weak self] data, _, _ in
-            guard let self = self, let data = data, let image = UIImage(data: data) else {
-                DispatchQueue.main.async { completion(nil) }
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard
+                let url = URL(string: "https://cdn.discordapp.com/emojis/\(id).png?v=1"),
+                let data = try? Data(contentsOf: url),
+                let image = UIImage(data: data)
+            else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
                 return
             }
 
-            // Cache and return
-            self.cacheQueue.async {
-                self.memoryCache.setObject(image, forKey: cacheKey)
-                try? data.write(to: URL(fileURLWithPath: filePath), options: .atomic)
-                DispatchQueue.main.async {
-                    completion(image)
-                }
-            }
-        }.resume()
-    }
+            self.memoryCache.setObject(image, forKey: id as NSString)
+            try? data.write(
+                to: URL(fileURLWithPath: self.cacheDirectory + "/" + id + ".png"),
+                options: .atomic
+            )
 
-    func clearCache() {
-        cacheQueue.async {
-            self.memoryCache.removeAllObjects()
-            let fileManager = FileManager.default
-            if let files = try? fileManager.contentsOfDirectory(atPath: self.cacheDirectory) {
-                for file in files where file.hasSuffix(".png") {
-                    try? fileManager.removeItem(atPath: self.cacheDirectory + "/" + file)
-                }
+            DispatchQueue.main.async {
+                completion(image)
             }
         }
     }
